ef592a7a77ef59a93292174afea8c21e3f4a29f1
Mathis <mathis.pati@gmail.com>
2022-01-10 22:09:44 +0100

    set up webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: './src/index.html',
+        }),
+    ],
+    module: {
+        rules: [
+            // ES6
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                      presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            // css
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            // sass, scss
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            // images
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    devServer: {
+        static: './dist',
+        hot: true,
+        open: true
+    }
+
+}
