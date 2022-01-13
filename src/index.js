199b436fe925fba6f3e175664ef3e6c0eaaa3371
Mathis <mathis.pati@gmail.com>
2022-01-13 01:07:27 +0100

    style all sections

--- src/index.js
+++ src/index.js
@@ -1,34 +1,62 @@
 import './styles/reset.css';
 import './styles/index.scss';
+import githubLogo from './assets/img/logo-github.png';
+import linkedinLogo from './assets/img/logo-linkedin.png';
 
-const sectionsContainer = document.querySelector('.container');
-const sections = document.querySelectorAll('.section');
-const sectionsContainerHeight = sectionsContainer.offsetHeight;
-let currentSectionIndex = 0;
+const app = {
+    sectionsContainer: document.querySelector('.container'),
+    currentSectionIndex: 0,
+    sections: document.querySelectorAll('.section'),
+    isScrolling: false,
 
-window.addEventListener('wheel', scrollToSection);
-sectionsContainer.addEventListener('transitionend', () => {
-    removeOrAddWheelListener(true);
-})
+    init() {
+        window.addEventListener('wheel', app.scrollToSection);
+        app.sectionsContainer.addEventListener('transitionend', () => {
+            app.setIsScrolling(false);
+        });
+        app.addHeaderImages();
+    },
 
-function scrollToSection(e) {
-    if(e.deltaY > 0) {
-        // stop if user is on the last section
-        if(sections.length - 1 === currentSectionIndex) return;
-        currentSectionIndex++;
-        sectionsContainer.style.transform = `translateY(${currentSectionIndex * -sectionsContainerHeight}px)`;
-        removeOrAddWheelListener(false);
+    scrollToSection(e) {
+        if(app.isScrolling) return;
+        if(e.deltaY > 0) {
+            // stop if user is on the last section
+            if(app.sections.length - 1 === app.currentSectionIndex) return;
+            app.currentSectionIndex++;
+            app.sectionsContainer.style.transform = `translateY(${app.currentSectionIndex * -app.sectionsContainer.offsetHeight}px)`;
+            app.setIsScrolling(true);
+        }
+        if(e.deltaY < 0) {
+            // stop if user is on the first section
+            if(app.currentSectionIndex === 0) return;
+            app.currentSectionIndex--;
+            app.sectionsContainer.style.transform = `translateY(${app.currentSectionIndex * -app.sectionsContainer.offsetHeight}px)`;
+            app.setIsScrolling(true);
+        }
+    },
+
+    setIsScrolling(bool) {
+        app.isScrolling = bool;
+    },
+
+    addHeaderImages() {
+        const imagesContainer = document.querySelector('.header__links');
+        const imgArr = [
+            {src: linkedinLogo, alt: 'lien compte linkedin', link: 'https://www.linkedin.com/in/mathis-pati/'},
+            {src: githubLogo, alt: "lien compte github", link: 'https://github.com/Mathis-web'}
+        ];
+        imgArr.forEach(el => {
+            const img = document.createElement('img');
+            const link = document.createElement('a');
+            link.href = el.link;
+            link.className = "header__links__item";
+            img.src = el.src;
+            img.alt = el.alt;
+            link.appendChild(img);
+            imagesContainer.appendChild(link);
+        });
+
     }
-    if(e.deltaY < 0) {
-        // stop if user is on the first section
-        if(currentSectionIndex === 0) return;
-        currentSectionIndex--;
-        sectionsContainer.style.transform = `translateY(${currentSectionIndex * -sectionsContainerHeight}px)`;
-        removeOrAddWheelListener(false);
-    }
-}
+};
 
-function removeOrAddWheelListener(bool) {
-    if(bool) window.addEventListener('wheel', scrollToSection);
-    else window.removeEventListener('wheel', scrollToSection);
-}
+app.init();
